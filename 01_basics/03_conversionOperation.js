e527df30c80af29b5d120e53f0ca2989d069c957
Abhay Chabuk <chabukabhay@gmail.com>
2025-03-05 07:30:01 +0000

    Got to know more about the operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -23,8 +23,8 @@ let isLoggedIn = "Abhay"
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
 
 //******************* Operations ****************
 
@@ -39,4 +39,30 @@ let negValue = -value
 // console.log(2/2);
 // console.log(2%3);
 
+let str1 = "hello"
+let str2 = " Abhay"
+
+let str3 = str1 + str2
+// console.log(str3)
+
+let str4 = str2 + str1
+// console.log(str4)
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+console.log(+true);
+// console.log(true+);  //Gives error
+
+console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
 
