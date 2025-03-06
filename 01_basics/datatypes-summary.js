ebbdff08651fc365e14b3b12456e22bba821d777
Abhay Chabuk <chabukabhay@gmail.com>
2025-03-06 14:42:16 +0000

    Summarized Data types and learnt about typeof function.

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,51 @@
+// Primitive 
+
+// 7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.23
+
+const isLoggedIn = true
+const outsideTemp = null
+let username;
+
+// console.log(username);  // undefined
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+// console.log(id === anotherId);  // False
+
+const bigNumber = 90212367062346478543223n   // BigInt
+
+
+
+// Reference (Non Primitive)
+
+// Array, Objects, Functions
+
+
+// Array
+const heros = ["Dwyane Johnson", "Ryan Gosling", "Will Smith"];
+
+
+// Object
+let myObj = {
+    name : "Abhay",
+    age : 21,
+}
+
+
+//Function
+const myFunction = function(){
+    console.log("My name is Abhay")
+}
+
+
+// Function to find datatype of variable
+
+console.log(typeof bigNumber);
+console.log(typeof myFunction);
+console.log(typeof heros);
+console.log(typeof myObj);
+
