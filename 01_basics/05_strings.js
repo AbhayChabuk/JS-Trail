65463b37f1529319187c0ac2eee7fc4060257c00
Abhay Chabuk <chabukabhay@gmail.com>
2025-03-07 17:40:58 +0000

    Got to know about strings.

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,41 @@
+const name = "Abhay"
+const num = 23
+
+// console.log(name + num);
+
+// modern way to represent string concatenation that is string interpolation
+console.log(`Hello my name is ${name} and my favourite number is ${num}`);
+
+// Another way to declare string
+const gameName = new String ('Efootball')
+console.log(gameName)
+
+// console.log(gameName[4]);
+// console.log(gameName.search("o"));
+// console.log(gameName.__proto__);
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+
+// charAt & indexOf
+console.log(gameName.charAt(5));
+console.log(gameName.indexOf('t'));
+
+//  substring
+const newString = gameName.substring(0, 4)
+console.log(newString);
+
+// slice
+const anotherString = gameName.slice(-8, 4)
+console.log(anotherString);
+
+
+const newName = "    Abhay    "
+console.log(newName);
+console.log(newName.trim());
+
+
+
+
+
+
+
