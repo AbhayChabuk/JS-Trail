523e6d88307368299809f8bf2436340c434b77ed
Abhay Chabuk <chabukabhay@gmail.com>
2025-03-07 18:17:39 +0000

    Starting with maths

--- 01_basics/06_nums_and_maths.js
+++ 01_basics/06_nums_and_maths.js
@@ -1,23 +1,27 @@
 const score = 100
-console.log(score);
+// console.log(score);
 
 
 const balance = new Number(100)
-console.log(balance);
-console.log(`Type of balance is ${typeof(balance)}`);
+// console.log(balance);
+// console.log(`Type of balance is ${typeof(balance)}`);
 
-console.log(balance.toString().length);
+// console.log(balance.toString().length);
 
 // Precision value
-console.log(balance.toFixed(2));
+// console.log(balance.toFixed(2));
 
 const value = 123.56346
-console.log(value.toPrecision(3));     // o/p : 124
-console.log(value.toPrecision(2));     // o/p : 1.2e+2
+// console.log(value.toPrecision(3));     // o/p : 124
+// console.log(value.toPrecision(2));     // o/p : 1.2e+2
 
 // Local Standards
 const hundreds = 1000000
 
-console.log(hundreds.toLocaleString());   // o/p : 1,000,000. in Js local standards are set  as per US
+// console.log(hundreds.toLocaleString());   // o/p : 1,000,000. in Js local standards are set  as per US
 // so to get in Indian standards we use
-console.log(hundreds.toLocaleString('en-IN'));
+// console.log(hundreds.toLocaleString('en-IN'));
+
+
+// ++++++++++++++++++++++++++ Maths ++++++++++++++++++++++++++++++++++++++++++++
+
