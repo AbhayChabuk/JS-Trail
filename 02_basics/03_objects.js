699daf73148e8d7f2f72c3deac4c52f7c0d3d1cc
Abhay Chabuk <chabukabhay@gmail.com>
2025-03-10 17:51:43 +0000

    Got to know how to access elements from the objects using multiple ways

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -3,6 +3,16 @@
 // object literals
 
 const JsUser = {
-    name : "Abhay"
-    
+    name : "Abhay",
+    age : 21,
+    location: "Talegaon Dabhade",
+    email: "chabukabhay@gmail.com",
+    isLoggedIn: false,
+    lastLoginDays: ["Sunday", "Monday"]
 }
+
+console.log(JsUser.email);   // not more suitable to work
+// o/p : chabukabhay@gmail.com
+
+console.log(JsUser["email"]);
+// o/p : chabukabhay@gmail.com
